86211b93118a55299b6629a487be05dac6426e87
Gradie <muhi0024@algonquinlive.com>
2024-09-23 12:23:06 -0400

    Finished constructors

--- week2.cpp
+++ week2.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+//add namespcae(package) called CST8219 here
+class Vehicle {
+	int numWheels;
+	int numDoors;
+
+public:
+	Vehicle() : Vehicle(4) {
+	
+	}
+
+	Vehicle(int w) : Vehicle ( w, 4) { 
+		numWheels = w; 
+	}
+
+	Vehicle(int w, int d) { 
+		numWheels = w; 
+		numDoors = d; 
+	}
+
+};
+
 int main(int argc, char **argv)
 {
    cout << "Hello World!" << endl;
